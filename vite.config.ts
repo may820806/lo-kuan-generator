a118e4d53d471e6b238a2d49c0df6e46cfd0d93b
may.w <may.>
2025-05-21 23:31:10 +0800

    trigger action

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './' // build完的index.html 會去哪裡找assets
+  base: './', // build完的index.html 會去哪裡找assets
+  server: {
+    port: 80,
+  } 
 })
